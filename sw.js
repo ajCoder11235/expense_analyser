ffa2df99949344c399c47c69f3052a13092b0cc0
ajCoder11235 <ajaz030507@gmail.com>
2025-10-25 01:49:16 +0530

    Expense Tracker

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'expense-analyzer-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  'httpss://cdn.jsdelivr.net/npm/chart.js',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+// Install the service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activate the service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Fetch events
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
